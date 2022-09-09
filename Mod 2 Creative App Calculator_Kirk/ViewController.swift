75e04a6cc0363bf4684ceefbf2bf3013b549d841
DJCJ14 <dekfrek@gmail.com>
2022-09-09 07:33:05 -0500

    initial commit

--- Mod 2 Creative App Calculator_Kirk/ViewController.swift
+++ Mod 2 Creative App Calculator_Kirk/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBOutlet weak var resultOutlet: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addAction(_ sender: Any) {
+        let num1 = Double (textField1.text!)!
+        let num2 = Double (textField2.text!)!
+        let sum = num1 + num2
+        resultOutlet.text = "\(sum)"
+        textField1.resignFirstResponder()
+        textField2.resignFirstResponder()
+    }
+    @IBAction func subtractAction(_ sender: Any) {
+        let num1 = Double (textField1.text!)!
+        let num2 = Double (textField2.text!)!
+        let difference = num1 - num2
+        resultOutlet.text = "\(difference)"
+        textField1.resignFirstResponder()
+        textField2.resignFirstResponder()
+    }
+    
+    @IBAction func perimeterAction(_ sender: Any) {
+        let length = Double (textField1.text!)!
+        let width = Double (textField2.text!)!
+        let perimeter = 2*(length + width)
+        resultOutlet.text = "\(perimeter)"
+        textField1.resignFirstResponder()
+        textField2.resignFirstResponder()
+    }
+    
 
 }
 
